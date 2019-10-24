1661166073d2fcbfc8256831b51ca43cceadc286
bandura <banduraweb@ukr.net>
2019-10-24 16:15:04 +0300

    hw1

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+    return (
+
+       <>
+           <Header />
+           <Content />
+           <Footer />
+       </>
+
+    );
+}
+
+export default App;
